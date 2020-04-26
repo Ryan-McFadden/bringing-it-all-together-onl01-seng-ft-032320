11b43e75d61ac837bfa81f982ff6376d9ab9f64a
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-26 20:28:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -94,7 +94,7 @@ class Dog
 
     if !dog.empty?
       dog = dog[0]
-      new_dog = Dog.new(id:dog[0],name:dog[1],breed:dog[2])
+      new_dog = Dog.new(id:dog[0], name:dog[1], breed:dog[2])
     else
       new_dog = self.create(name:name,breed:breed)
     end
