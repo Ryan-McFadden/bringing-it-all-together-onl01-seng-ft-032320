5aa0348e38d84f566f42c3b4395fb8a722f7af26
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-26 20:22:08 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,112 @@
+class Dog
+  attr_accessor :name, :breed, :id
+  
+  def initialize(hash)
+    
+    hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+    self.id ||= nil
+    
+  end 
+  
+  def self.create_table
+    
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+      SQL
+      
+    DB[:conn].execute(sql)
+    
+  end
+  
+  def self.drop_table
+    
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+      SQL
+      
+    DB[:conn].execute(sql)
+    
+  end
+  
+  def save
+    
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+      
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    
+    self
+    
+  end
+  
+  def self.create(hash)
+    
+    new_dog = self.new(hash)
+    new_dog.save
+    
+    new_dog
+    
+  end
+  
+  def self.new_from_db(row)
+    
+    hash = {
+      id: row[0],
+      name: row[1],
+      breed: row[2]
+    }
+    
+    self.new(hash)
+    
+  end
+  
+  def self.find_by_id(id)
+    
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+      SQL
+      
+    DB[:conn].execute(sql, id).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.find_or_create_by(hash)
+    
+    
+    
+  end
+  
+  def self.find_by_name(name)
+    
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      SQL
+      
+    DB[:conn].execute(sql, name).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+      SQL
+      
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+end
